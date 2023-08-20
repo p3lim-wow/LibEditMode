af109068792c6d94332ad7f1addd197bdee78d7f
Adrian L Lange <git@p3lim.net>
2023-08-20 22:36:58 +0200

    Add override for label name

--- LibEditMode.lua
+++ LibEditMode.lua
@@ -184,7 +184,7 @@ function lib:AddFrame(frame, callback, default)
 	selection:SetScript('OnMouseDown', onMouseDown)
 	selection:SetScript('OnDragStart', onDragStart)
 	selection:SetScript('OnDragStop', onDragStop)
-	selection.Label:SetText(frame:GetName())
+	selection.Label:SetText(frame.editModeName or frame:GetName())
 	selection:Hide()
 
 	frameSelections[frame] = selection
