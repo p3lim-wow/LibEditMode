f4b9509d15b0435cbe84a40b7420f8e942e6dfc8
Adrian L Lange <git@p3lim.net>
2025-10-11 16:18:24 +0200

    Need to check for nil on booleans
    Fixes #32

--- widgets/checkbox.lua
+++ widgets/checkbox.lua
@@ -9,14 +9,19 @@ function checkboxMixin:Setup(data)
 	self.setting = data
 	self.Label:SetText(data.name)
 
-	self.checked = data.get(lib.activeLayoutName) or data.default
-	self.Button:SetChecked(not not self.checked)
+	local value = data.get(lib.activeLayoutName)
+	if value == nil then
+		value = data.default
+	end
+
+	self.checked = value
+	self.Button:SetChecked(not not value) -- force boolean
 end
 
 function checkboxMixin:OnCheckButtonClick()
 	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
 	self.checked = not self.checked
-	self.setting.set(lib.activeLayoutName, not not self.checked)
+	self.setting.set(lib.activeLayoutName, not not self.checked) -- force boolean
 end
 
 lib.internal:CreatePool(lib.SettingType.Checkbox, function()
