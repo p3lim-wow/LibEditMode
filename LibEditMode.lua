a8e609da306d9c5c86f04addae32c85dee2b2fd0
Adrian L Lange <git@p3lim.net>
2025-12-14 14:57:49 +0100

    Update dialog on layout changes
    Fixes #60

--- LibEditMode.lua
+++ LibEditMode.lua
@@ -213,6 +213,12 @@ local function onEditModeChanged(_, layoutInfo)
 		-- update cache
 		lib.layoutCache = C_EditMode.GetLayouts().layouts
 
+		-- update dialog
+		if internal.dialog.selection then
+			internal.dialog:Update(internal.dialog.selection)
+		end
+
+		-- trigger callbacks
 		for _, callback in next, lib.anonCallbacksLayout do
 			securecallfunction(callback, layoutNames[activeLayout], activeLayout)
 		end
